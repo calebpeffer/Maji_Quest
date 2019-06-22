c472c81b8ea4d95236140af338019a601f085385
Nicolas <nicolascamara29@gmail.com>
2019-06-22 13:52:21 -0400

    Object Class

--- event.py
+++ event.py
@@ -3,11 +3,13 @@ class Event():
         self.correspondingEvents = correspondingEvents
         self.objects = objects
         self.characters = characters
-        self.name= name
-        self.description= description
+        self.name = name
+        self.description = description
         self.event_id = event_id
         self.map_next = map_next
         self.map_prev = map_prev
+
+    
 
             
   
